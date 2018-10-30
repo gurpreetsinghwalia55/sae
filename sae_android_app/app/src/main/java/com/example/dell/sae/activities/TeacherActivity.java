d4cfc2c1393eda26966104d7765ef002dff74edc
root <root@Walia.localdomain>
2018-10-30 12:42:31 +0530

    Added TeacherActivity to android app

---
+++ sae_android_app/app/src/main/java/com/example/dell/sae/activities/TeacherActivity.java
@@ -0,0 +1,85 @@
+package com.example.dell.sae.activities;
+
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.dell.sae.R;
+import com.example.dell.sae.fragments.TeacherProfileFragment;
+
+public class TeacherActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FragmentManager fragmentManager;
+    private Handler handler = new Handler();
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        fragmentManager = getSupportFragmentManager();
+
+        if (savedInstanceState == null) {
+            changeFragments(new TeacherProfileFragment());
+            navigationView.setCheckedItem(R.id.nav_profile);
+        }
+    }
+
+    private void changeFragments(final Fragment fragment) {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.teacher_fragment_container, fragment);
+                transaction.commit();
+            }
+        }, 500);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.teacher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_profile) {
+            changeFragments(new TeacherProfileFragment());
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ sae_android_app/app/src/main/java/com/example/dell/sae/fragments/TeacherProfileFragment.java
@@ -0,0 +1,38 @@
+package com.example.dell.sae.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dell.sae.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TeacherProfileFragment extends Fragment {
+
+    public TeacherProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_teacher_profile, container, false);
+        Toolbar toolbar = rootView.findViewById(R.id.teacher_profile_toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout = getActivity().findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        return rootView;
+    }
+
+}
